2037d0a894d2d97fa1478a25e36741c98e9be1f9
Artem Popov <artfwo@gmail.com>
2014-07-21 09:16:25 +0700

    hello example 'optimisation'.

--- examples/hello.py
+++ examples/hello.py
@@ -5,5 +5,5 @@ import aiosc
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(
-    aiosc.send(('127.0.0.1', 9000), '/hello', 'world', 1, 3.14, '2')
+    aiosc.send(('127.0.0.1', 9000), '/hello', 'world')
 )
